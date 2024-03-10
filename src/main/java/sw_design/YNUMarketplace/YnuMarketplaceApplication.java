379aeaf70cdfe8d6a98de9ba67fb22875e4c2d04
minju <minju9711@naver.com>
2024-03-10 18:43:50 +0900

    test : 커밋테스트

---
+++ src/main/java/sw_design/YNUMarketplace/YnuMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package sw_design.YNUMarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YnuMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YnuMarketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sw_design/YNUMarketplace/YnuMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package sw_design.YNUMarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YnuMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
