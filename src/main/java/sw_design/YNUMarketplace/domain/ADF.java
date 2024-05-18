2b1ae416db4d13d8401e2ecd2721cafe1811597e
minju <minju9711@naver.com>
2024-05-18 17:13:49 +0900

    add : ãdb

--- src/main/java/sw_design/YNUMarketplace/domain/ADF.java
+++
@@ -1,4 +0,0 @@
-package sw_design.YNUMarketplace.domain;
-
-public class ADF {
-}
---
+++ src/main/java/sw_design/YNUMarketplace/domain/user/model/User.java
@@ -0,0 +1,13 @@
+package sw_design.YNUMarketplace.domain.user.model;
+
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+}
