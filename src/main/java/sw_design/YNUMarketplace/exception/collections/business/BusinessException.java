db4510ca832b6bb6e5b80bed63bc67a379d890cb
minju <minju9711@naver.com>
2024-05-20 19:26:54 +0900

    add : 비즈니스 로직 에러 처리를 담당할 상위 클래스 설정

---
+++ src/main/java/sw_design/YNUMarketplace/exception/collections/business/BusinessException.java
@@ -0,0 +1,8 @@
+package sw_design.YNUMarketplace.exception.collections.business;
+
+public class BusinessException extends RuntimeException{
+    protected BusinessException(String message) {
+        super(message);
+    }
+
+}
