3e77280022c5670c9976ad3fbae0cbae8e2fdbd4
minju <minju9711@naver.com>
2024-05-20 20:50:59 +0900

    rename : 클래스명 수정

--- src/main/java/sw_design/YNUMarketplace/exception/message/AuthExceptionMessage.java
+++ src/main/java/sw_design/YNUMarketplace/exception/message/AuthExceptionMessage.java
@@ -3,6 +3,6 @@ package sw_design.YNUMarketplace.exception.message;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-public class authMessage {
+public class AuthExceptionMessage {
     public static final String UnAuthenticatedUserAccess=  "인증되지 않은 사용자입니다.";
 }
