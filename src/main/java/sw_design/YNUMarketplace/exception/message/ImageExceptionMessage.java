90284ed446829dac1c0768e46c338e5a5483e73d
minju <minju9711@naver.com>
2024-05-23 01:41:09 +0900

    feat : 이미지 파일 입출력시 발생될 수 있는 에러에 대한 처리 클래스

---
+++ src/main/java/sw_design/YNUMarketplace/exception/message/ImageExceptionMessage.java
@@ -0,0 +1,6 @@
+package sw_design.YNUMarketplace.exception.message;
+
+public class ImageExceptionMessage {
+    public static final String NoExitsInitImageFile = "기본 프로필 사진의 위치를 찾을 수 없습니다.";
+    public static final String AllImageFileIOError = "파일 입출력 과정에서 오류가 발생했습니다.";
+}
