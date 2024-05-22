a8c652161e8383e0e1f4392765e98e28c30eb46e
minju <minju9711@naver.com>
2024-05-23 01:35:23 +0900

    feat : 클라이언트에 대한 요청처리 완료 시, 반환해줄 메시지 상수 클래스

---
+++ src/main/java/sw_design/YNUMarketplace/config/contant/ControllerConstants.java
@@ -0,0 +1,7 @@
+package sw_design.YNUMarketplace.config.contant;
+
+public class ControllerConstants {
+    public static final String completeSignUp = "회원가입이 완료되었습니다.";
+    public static final String completeSendVerifyNum = "인증번호 전송이 완료되었습니다.";
+    public static final String completeCheckVerifyNum = "인증번호 확인이 완료되었습니다.";
+}
