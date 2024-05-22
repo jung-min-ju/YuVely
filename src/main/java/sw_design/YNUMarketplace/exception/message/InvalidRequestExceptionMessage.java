f8bd73ad18c87d9d82bcc3f9ca89bbe28e3f4615
minju <minju9711@naver.com>
2024-05-23 01:44:31 +0900

    feat : client의 잘못된 입력값이 주어졌을 때의 에러처리 클래스

---
+++ src/main/java/sw_design/YNUMarketplace/exception/message/InvalidRequestExceptionMessage.java
@@ -0,0 +1,5 @@
+package sw_design.YNUMarketplace.exception.message;
+
+public class InvalidRequestExceptionMessage {
+    public static final String BindingErrorMessage = "필드값이 유효하지 않습니다. 올바르 필드값을 입력해주세요.";
+}
