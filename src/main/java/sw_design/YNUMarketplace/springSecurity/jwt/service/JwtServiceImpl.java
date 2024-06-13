c0f9c892c77766a94853f128f0c3263d985ad549
minju <minju9711@naver.com>
2024-06-13 22:56:46 +0900

    modify : 변수명 수정

--- src/main/java/sw_design/YNUMarketplace/springSecurity/jwt/service/JwtServiceImpl.java
+++ src/main/java/sw_design/YNUMarketplace/springSecurity/jwt/service/JwtServiceImpl.java
@@ -20,11 +20,13 @@ public class JwtServiceImpl implements JwtService{
     @Override
     public AccessTokenDto updateAccessToken(HttpServletRequest request) {
         //리프래시 토큰이 있는지 검증
-        String refreshToken = jwtTokenProvider.resolveAccessToken(request);
+        String refreshToken = jwtTokenProvider.resolveToken(request);
         if(refreshToken.equals(null)) throw new TokenMissingException();
 
+        boolean isRefreshTokenValid = jwtTokenProvider.validateRefreshTokenInRedis(refreshToken);
+
         //해당 토큰이 레디스에 존재하는지 + 토큰 자체의 유효성 검증
-        if (!jwtTokenProvider.validateRefreshTokenInRedis(refreshToken)) throw new RefreshTokenExpirationException();
+        if (!isRefreshTokenValid) throw new RefreshTokenExpirationException();
 
         TokenSubAndRoleDto tokenSubAndRoleDto = jwtTokenProvider.getSubjectAndRoleFromRefresh(refreshToken);
         return jwtTokenProvider.createAccessToken(tokenSubAndRoleDto.getAccountId(), tokenSubAndRoleDto.getRole());
