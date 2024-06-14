6dbda9bb4d40824e768b213891177241f3f67968
minju <minju9711@naver.com>
2024-06-14 22:32:20 +0900

    modify : 영남대 이메일 형식 추가

--- src/main/java/sw_design/YNUMarketplace/businessProcess/auth/dto/smtp/EmailDto.java
+++ src/main/java/sw_design/YNUMarketplace/businessProcess/auth/dto/smtp/EmailDto.java
@@ -17,6 +17,6 @@ public class EmailDto {
     @NotBlank(message = "이메일은 필수 값입니다.")
     @Email(message = "유효한 이메일 주소를 입력해주세요.")
     @Size(min = 5, max = 254, message = "5이상, 254이하 이메일을 입력해주세요.")
-    @Pattern(regexp = "^[\\w.%+-]+@yu\\.ac\\.kr$", message = "영남대학교 전용 이메일을 입력해주세요.")
+    @Pattern(regexp = "^[\\w.%+-]+@(yu|ynu)\\.ac\\.kr$", message = "영남대학교 전용 이메일을 입력해주세요.")
     private String email;
 }
--- src/main/java/sw_design/YNUMarketplace/businessProcess/auth/dto/verification/VerificationEmailDto.java
+++ src/main/java/sw_design/YNUMarketplace/businessProcess/auth/dto/verification/VerificationEmailDto.java
@@ -16,7 +16,7 @@ public class VerificationEmailDto {
     @NotBlank(message = "이메일은 필수 값입니다.")
     @Email(message = "유효한 이메일 주소를 입력해주세요.")
     @Size(min = 5, max = 254, message = "5이상, 254이하 이메일을 입력해주세요.")
-    @Pattern(regexp = "^[\\w.%+-]+@yu\\.ac\\.kr$", message = "영남대학교 전용 이메일을 입력해주세요.")
+    @Pattern(regexp = "^[\\w.%+-]+@(yu|ynu)\\.ac\\.kr$", message = "영남대학교 전용 이메일을 입력해주세요.")
     private String email;
 
     @NotBlank(message = "이메일 인증번호를 입력해주세요.")
